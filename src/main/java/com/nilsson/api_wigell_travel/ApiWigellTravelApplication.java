7cc4bb625ee201e14aabc4a26a0990d2f8e16ebf
Sara Nilsson <nilssonsara97@gmail.com>
2026-03-30 09:46:22 +0200

    First commit

---
+++ src/main/java/com/nilsson/api_wigell_travel/ApiWigellTravelApplication.java
@@ -0,0 +1,13 @@
+package com.nilsson.api_wigell_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWigellTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWigellTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nilsson/api_wigell_travel/ApiWigellTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilsson.api_wigell_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWigellTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
